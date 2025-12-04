543e5974dd9c6ef10e9ecfb570a2b7d3917decdf
Keaton Taylor <keatonstaylor@gmail.com>
2025-12-04 11:43:55 -0700

    error handling

--- sxm.py
+++ sxm.py
@@ -172,22 +172,26 @@ class SiriusXM:
         if not data:
             return None
 
+        try:
+            status = data['ModuleListResponse']['status']
+            station = musicdata['markerLists'][0]['markers'][0]['episode']['longTitle']
 
-        status = data['ModuleListResponse']['status']
-        musicdata = data['ModuleListResponse']['moduleList']['modules'][0]['moduleResponse']['liveChannelData']
-        station = musicdata['markerLists'][0]['markers'][0]['episode']['longTitle']
-        self.current_metadata = musicdata['markerLists'][-1]['markers'][-1]['cut']
+            musicdata = data['ModuleListResponse']['moduleList']['modules'][0]['moduleResponse']['liveChannelData']
+            self.current_metadata = musicdata['markerLists'][-1]['markers'][-1]['cut']
 
-        data_to_log = {
-            'title': musicdata['markerLists'][-1]['markers'][-1]['cut']['title'],
-            'artist': musicdata['markerLists'][-1]['markers'][-1]['cut']['artists'][0]['name'],
-            'station': station,
-            'playing': True,
-        }
-        self.current_title = data_to_log["title"]
-        self.current_artist = data_to_log["artist"]
-        message = data['ModuleListResponse']['messages'][0]['message']
-        message_code = data['ModuleListResponse']['messages'][0]['code']
+            data_to_log = {
+                'title': musicdata['markerLists'][-1]['markers'][-1]['cut']['title'],
+                'artist': musicdata['markerLists'][-1]['markers'][-1]['cut']['artists'][0]['name'],
+                'station': station,
+                'playing': True,
+            }
+            self.current_title = data_to_log["title"]
+            self.current_artist = data_to_log["artist"]
+            message = data['ModuleListResponse']['messages'][0]['message']
+            message_code = data['ModuleListResponse']['messages'][0]['code']
+        except (KeyError, IndexError):
+            self.log('Error parsing json response for playlist')
+            return None
 
 
         # login if session expired
